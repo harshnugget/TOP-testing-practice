3cb0cf9aaaf1f36b92b1c9eb494bbb10349fc400
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-09-13 06:33:31 +0100

    feat: add capitalization support to caesarCipher function
    - Added support for capital letters in the caesarCipher function
    - Updated tests to cover capitalization scenarios
    
    To be added:
        - Support for negative shifting

--- caesarCipher.js
+++ caesarCipher.js
@@ -1,19 +1,25 @@
 const caesarCipher = (() => {
   const alphabet = "abcdefghijklmnopqrstuvwxyz";
 
+  function isUpperCase(letter) {
+    return letter === letter.toUpperCase();
+  }
+
   return (string, shift) => {
     let encryptedString = "";
 
     // Iterate through each letter of the provided string
     for (let i = 0; i < string.length; i++) {
       // Get the corresponding index of the current letter from "alphabet"
-      let index = alphabet.indexOf(string[i]);
+      let index = alphabet.indexOf(string[i].toLowerCase());
 
       // Get index after shifting the current letter
       let shiftedIndex = (index + shift) % alphabet.length;
 
       // Retrieve the corresponding letter from "alphabet" after the shift
-      let letter = alphabet[shiftedIndex];
+      let letter = isUpperCase(string[i])
+        ? alphabet[shiftedIndex].toUpperCase()
+        : alphabet[shiftedIndex];
 
       // Append the letter to the encrypted string
       encryptedString += letter;
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -4,4 +4,9 @@ describe("Caesar cipher", () => {
   test("Test wrapping", () => {
     expect(caesarCipher("xyz", 3)).toBe("abc");
   });
+
+  test("Test mixed-case strings", () => {
+    expect(caesarCipher("HeLLo", 3)).toBe("KhOOr");
+    expect(caesarCipher("HelloWorld", 13)).toBe("UryybJbeyq");
+  });
 });
